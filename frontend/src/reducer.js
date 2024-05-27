a4cac956384b09e9dd2e95ac2bc6626690f16048
natnaelgebremichael <natnael92460@gmail.com>
2024-05-27 15:59:00 +1200

    initial Setup

---
+++ frontend/src/reducer.js
@@ -0,0 +1,10 @@
+import { combineReducer } from 'redux'
+
+//Import reducers here
+// import authReducer from './authReducer';
+// import stockReducer from './stockReducer';
+
+const rootReducer = combineReducer({
+    //Auth: authReducer,
+    //Stock: stockReducer,
+})
