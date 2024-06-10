ccabb091d521f2d752ef51993d44551a282ad200
natnaelgebremichael <natnael92460@gmail.com>
2024-06-10 15:20:04 +1200

    Updated GA and Clarity ID

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,12 +6,12 @@ import { useEffect } from 'react';
 import { clarity } from 'react-microsoft-clarity'
 import { Route, Routes } from 'react-router-dom';
 
-// OUR_TRACKING_ID
-const TRACKING_ID = import.meta.env.GA_TRACKING_ID || ""; 
-ReactGA.initialize(TRACKING_ID);
-
+const CLARITY_ID = import.meta.env.VITE_CLARITY_ID || ""; 
+const TRACKING_ID = import.meta.env.VITE_GA_TRACKING_ID || ""; 
 const NODE_ENV = import.meta.env.VITE_NODE_ENV || "";
-if (NODE_ENV === 'production') clarity.init('midum145ht');
+
+ReactGA.initialize(TRACKING_ID);
+if (NODE_ENV === 'production') clarity.init(CLARITY_ID);
 
 
 function App() {
