198e5778ba5264a1da06ed22b368cd185a717890
natnaelgebremichael <natnael92460@gmail.com>
2024-06-10 14:49:22 +1200

    Check APi

--- frontend/src/components/Products/products.tsx
+++ frontend/src/components/Products/products.tsx
@@ -1,30 +1,32 @@
-import { useFetchProductsQuery } from '../../features/products/product-api-slice';
-import ProductList from './productList';
+// import { useFetchProductsQuery } from '../../features/products/product-api-slice';
+// import ProductList from './productList';
 
 const Products = () => {
-    const { data: products = [], isLoading, error } = useFetchProductsQuery();
+    const STOCK_BASE_URL = import.meta.env.VITE_STOCK_BASE_URL;
+    // const { data: products = [], isLoading, error } = useFetchProductsQuery();
 
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
+    // if (isLoading) {
+    //     return <div>Loading...</div>;
+    // }
 
-    if (error) {
-        let errorMessage = 'An error occurred.';
-        if ('status' in error) {
-            // You can access the error status and data if needed
-            const errorStatus = error.status;
-            const errorData = error.data;
-            errorMessage = `Error ${errorStatus}: ${JSON.stringify(errorData)}`;
-        } else if ('message' in error) {
-            errorMessage = error.message as string;
-        }
-        return <div>Error: {errorMessage}</div>;
-    }
+    // if (error) {
+    //     let errorMessage = 'An error occurred.';
+    //     if ('status' in error) {
+    //         // You can access the error status and data if needed
+    //         const errorStatus = error.status;
+    //         const errorData = error.data;
+    //         errorMessage = `Error ${errorStatus}: ${JSON.stringify(errorData)}`;
+    //     } else if ('message' in error) {
+    //         errorMessage = error.message as string;
+    //     }
+    //     return <div>Error: {errorMessage}</div>;
+    // }
 
 
     return (
         <div>
-            <ProductList products={products} />
+            <p>Hi Products Page,{STOCK_BASE_URL} </p>
+            {/* <ProductList products={products} /> */}
         </div>
     );
 };
