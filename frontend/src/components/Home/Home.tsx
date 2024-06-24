db238748472ecdca8923a8fa0f3aa65fd4949f80
natnaelgebremichael <natnael92460@gmail.com>
2024-06-25 10:11:38 +1200

    Updated unnecessary page logs for GA$

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,17 +1,9 @@
-import { logPageView } from '../../analytics';
 import useDocumentTitle from '../../useDocumentTitle';
 import { Footer, Header, Info, Topics } from '../index'
-
-import { useEffect } from 'react';
 
 const Home = () => {
 
     useDocumentTitle('Home Page - Stock + Spark');
-
-    useEffect(() => {
-        logPageView(location.pathname, document.title);
-    }, [location]);
-
 
     return (
         <div>
--- frontend/src/components/Products/products.tsx
+++ frontend/src/components/Products/products.tsx
@@ -1,18 +1,11 @@
 import './product.css'
 import { Navbar } from '../index'
 import ProductList from './productList';
-import { logPageView } from '../../analytics';
 import useDocumentTitle from '../../useDocumentTitle';
-
-
-import { useEffect } from 'react';
 
 const Products = () => {
 
     useDocumentTitle('Products Page - Stock + Spark');
-    useEffect(() => {
-        logPageView(location.pathname, document.title);
-    }, [location]);
 
     return (
         <div className='ProductsPage'>
